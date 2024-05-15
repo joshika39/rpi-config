f5d272bbfdfd63c0c9fc16fc7bc764f7bd3f96be
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-15 15:10:35 +0200

    added base copy script

---
+++ scripts/copy-ssh-keys.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script copies the SSH keys to the target machine
+# Usage: ./copy-ssh-keys.sh <target_ips> <username> <password> <ssh_key_path>
+
+# Check if the number of arguments is less than 4
+if [ $# -lt 4 ]; then
+    echo "Usage: ./copy-ssh-keys.sh <target_ips> <username> <password> <ssh_key_path>"
+    exit 1
+fi
+
+# Extract the arguments
+target_ips=$1
+username=$2
+password=$3
+ssh_key_path=$4
+
+IFS=',' read -r -a target_ips_array <<< "$target_ips"
+
+for target_ip in "${target_ips_array[@]}"
+do
+    ssh-copy-id -i $ssh_key_path $username@$target_ip
+done
