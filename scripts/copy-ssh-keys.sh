106e7b2fc66c346f57228af8235fc034aeb6b72c
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-15 20:21:39 +0200

    finished script

--- scripts/copy-ssh-keys.sh
+++ scripts/copy-ssh-keys.sh
@@ -5,7 +5,7 @@
 
 # Check if the number of arguments is less than 4
 if [ $# -lt 4 ]; then
-    echo "Usage: ./copy-ssh-keys.sh <target_ips> <username> <password> <ssh_key_path>"
+    echo "Usage: ./copy-ssh-keys.sh <target_ips> <username> <password> <ssh_key_path> [--force]"
     exit 1
 fi
 
@@ -14,10 +14,11 @@ target_ips=$1
 username=$2
 password=$3
 ssh_key_path=$4
+force=${5:-""}
 
 IFS=',' read -r -a target_ips_array <<< "$target_ips"
 
 for target_ip in "${target_ips_array[@]}"
 do
-    ssh-copy-id -i $ssh_key_path $username@$target_ip
+    ssh-copy-id -i $ssh_key_path $force $username@$target_ip
 done
